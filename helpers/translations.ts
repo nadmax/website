fbf32e9633fee6435b9b23bec902d9c9ce06f82c
Maximilien Nadji <maximilien.nadji@pm.me>
2025-04-04 13:01:16 +0200

    separate locale translation files (#140)

--- helpers/translations.ts
+++ helpers/translations.ts
@@ -4,15 +4,31 @@ import { Translations } from '../types/translations';
 
 const translationsCache: { [key: string]: Translations } = {};
 
-export async function loadTranslations(lang: string): Promise<Translations> {
-    if (translationsCache[lang]) {
-        return translationsCache[lang];
+export async function loadTranslations(lang: string, page: string): Promise<Translations> {
+    const cacheKey = `${lang}-${page}`;
+
+    if (translationsCache[cacheKey])
+        return translationsCache[cacheKey];
+
+    try {
+        const commonPath = path.resolve(__dirname, `../locales/${lang}/common.json`);
+        const pagePath = path.resolve(__dirname, `../locales/${lang}/${page}.json`);
+        const common = JSON.parse(await fs.promises.readFile(commonPath, "utf-8"));
+        let pageTranslations = {};
+        if (fs.existsSync(pagePath))
+            pageTranslations = JSON.parse(await fs.promises.readFile(pagePath, "utf-8"));
+        
+        const translations = {
+            common,
+            page: pageTranslations 
+        };
+
+        translationsCache[cacheKey] = translations;
+    
+        return translations;
+    } catch (error) {
+        console.error(`Error loading translations for ${lang}/${page}:`, error);
+
+        return {};
     }
-
-    const filePath = path.resolve(__dirname, `../locales/${lang}.json`);
-    const data = await fs.promises.readFile(filePath, 'utf-8');
-    const translations = JSON.parse(data);
-    translationsCache[lang] = translations;
-
-    return translations;
 }
